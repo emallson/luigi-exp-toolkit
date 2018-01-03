fabf1e000f9b6a3d653e6b160d9c1f78330a0c43
J David Smith <emallson@atlanis.net>
2018-01-03 15:44:43 -0500

    Pulled some updates in from an existing project

--- src/mixins.py
+++ src/mixins.py
@@ -3,13 +3,17 @@ import hashlib
 import warnings
 
 class ThreadsMixin(object):
+    static_resources = {}
     """Sets the thread usage based on the `threads` property.
     
     Overrides `resources`.
     """
     @property
     def resources(self):
-        return {"threads": self.threads}
+        return dict({"threads": self.threads}, **self.static_resources)
+
+def significant_params(task):
+    return {k: v for k, v in dict(task.get_params()).items() if v.significant}
 
 class HashOutputMixin(object):
     """Determine output path based on hash of parameters (ordered by parameter name).
@@ -23,18 +27,26 @@ class HashOutputMixin(object):
     `rename_logs.py` script to adjust log names after adding new
     parameters.
     """
-    def output(self, omit=set()):
+    def log_path(self, omit=set()):
         hash = hashlib.sha256()
-        for param, value in sorted(self.to_str_params(only_significant=True).items(), key=lambda t: t[0]):
+        for param, value in sorted(significant_params(self).items(), key=lambda t: t[0]):
             if param not in omit:
                 hash.update(param.encode('utf-8'))
-                hash.update(value.encode('utf-8'))
+                if hasattr(self.param_kwargs[param], "encode"):
+                    hash.update(self.param_kwargs[param].encode('utf-8'))
+                else:
+                    hash.update(str(self.param_kwargs[param]).encode('utf-8'))
+        return hash.hexdigest()
 
-        return luigi.LocalTarget(self.LOG_PATH.format(hash.hexdigest()))
+    def output(self, omit=set()):
+        return luigi.LocalTarget(self.LOG_PATH.format(self.log_path(omit)))
 
     def encode(self, locale):
         """Janky means to allow tasks of this kind as parameters to other HashOutputMixin Tasks."""
-        return self.output().path.encode(locale)
+        return self.log_path().encode(locale)
+
+    def __ser__(self):
+        return self.log_path()
 
 class DictOutputMixin(object):
     """Determine output path as a sequence of `_`-separated `key=value` pairs (ordered by parameter name).
@@ -50,29 +62,41 @@ class DictOutputMixin(object):
     """
     def sanitize(s):
         return s.replace("/", "-").replace("_","-")
-    def output(self, omit=set()):
+    def log_path(self, omit=set()):
         pairs = []
-        for param, value in sorted(self.to_str_params(only_significant=True).items(), key=lambda t: t[0]):
+        for param, value in sorted(significant_params(self).items(), key=lambda t: t[0]):
             if param not in omit:
-                pairs += ["{}={}".format(DictOutputMixin.sanitize(param), DictOutputMixin.sanitize(value))]
+                pairs += ["{}={}".format(DictOutputMixin.sanitize(param), DictOutputMixin.sanitize(value.serialize(self.param_kwargs[param])))]
+        return "_".join(pairs)
 
-        return luigi.LocalTarget(self.LOG_PATH.format("_".join(pairs)))
+    def output(self, omit=set()):
+        return luigi.LocalTarget(self.LOG_PATH.format(self.log_path(omit)))
 
     def encode(self, locale):
         """Janky means to allow tasks of this kind as parameters to other HashOutputMixin Tasks."""
-        return self.output().path.encode(locale)
+        return self.log_path().encode(locale)
+
+    def __ser__(self):
+        return self.log_path()
 
 class TempPathMixin(object):
+    TMP_KEY = None
     """Wraps `run()`, setting `self._tmp_output` to the path of a
     temporary file which will be moved to the location given by
     `output()` after the run completes.
     
     Only usable on tasks with a single output."""
     def run(self):
-        self.output().makedirs()
-        with self.output().temporary_path() as path:
-            self._tmp_output = path
-            super().run() 
+        if isinstance(self.output(), luigi.target.Target):
+            self.output().makedirs()
+            with self.output().temporary_path() as path:
+                self._tmp_output = path
+                super().run() 
+        else:
+            self.output()[self.TMP_KEY].makedirs()
+            with self.output()[self.TMP_KEY].temporary_path() as path:
+                self._tmp_output = path
+                super().run()
 
 class RustBacktraceMixin(object):
     """Turns on Rust backtraces. Only works for `ExternalProgramTask`
@@ -95,3 +119,8 @@ class TaskParameter(luigi.Parameter):
             return
         if not isinstance(param_value, self.kind):
             warnings.warn('Parameter "{}" with value "{}" is not an instance of {}'.format(param_name, param_value, self.kind))
+    def serialize(self, value):
+        if hasattr(value, "__ser__"):
+            return value.__ser__()
+        else:
+            return super().serialize(value)
